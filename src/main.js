0df1c213b00312b9c1d2a57bb2262f075b18b95a
aq5092 <alijandanwh3@gmail.com>
2025-11-10 11:23:22 +0500

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(store).use(router).mount('#app')
+loadFonts()
+// Telegram WebApp ma'lumotlarini saqlash
+if (window.Telegram?.WebApp) {
+  app.config.globalProperties.$tg = window.Telegram.WebApp
+  app.config.globalProperties.$user = window.Telegram.WebApp.initDataUnsafe.user
+}
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hr-bot-frontend/'  // O'zingizning repo nomingiz
+    : '/'
+}``
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+
+//   pluginOptions: {
+//     vuetify: {
+// 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+// 		}
+//   }
+// })
